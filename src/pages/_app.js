84fd4f646a93b3584cab1253d712e49e1d84c3ed
Stackbit Code Editor <projects@stackbit.com>
2022-06-04 16:49:50 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,48 +1,8 @@
-import Layout from "../components/Layout";
-import "../styles/globals.css";
-import { useEffect } from "react";
-import Script from "next/script";
-import { useRouter } from "next/router";
-import * as gtag from "../lib/gtag";
+import '../css/main.css';
+import Script from 'next/script'
 
-function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
 
-  return (
-    <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
 
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
-  );
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />;
 }
-
-export default MyApp;
