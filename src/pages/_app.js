37aa2f6a668d0968e069f6c162cb067b9bd364a8
Stackbit Code Editor <projects@stackbit.com>
2022-06-04 02:03:58 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
-import * as ga from '../lib/ga'
+import * as ga from '/lib/ga'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
