8a92c2dd646f943ffdad6d333eddf737d5ac25a7
Stackbit Code Editor <projects@stackbit.com>
2022-06-04 16:47:54 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,39 @@
 import '../css/main.css';
 import Script from 'next/script'
 
+function MyApp({ Component, pageProps }) {
+ 
 
+//...
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=XXXXXXXXX`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XXXXXXX', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+}
+
+export default MyApp;
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
