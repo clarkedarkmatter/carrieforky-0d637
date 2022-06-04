0accf6373b4de8635e6ab2167f348123584873b6
Stackbit Code Editor <projects@stackbit.com>
2022-06-04 02:02:24 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,10 +4,7 @@ export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
 
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 
-import * as ga from '../lib/ga'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
